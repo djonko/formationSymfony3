8d623202b381dc9569170c34fa7def6c5f3c0885
djonko <prevostdjonko@gmail.com>
2017-01-27 01:58:52 +0100

    debut Exercice

---
+++ src/OC/PlateformBundle/Antispam/OCAntispam.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 1/27/2017
+ * Time: 1:07 AM
+ */
+
+namespace OC\PlateformBundle\Antispam;
+
+
+class OCAntispam
+{
+
+    private $mailer;
+    private $locale;
+    private $minLength;
+
+    public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
+    {
+        $this->mailer    = $mailer;
+        $this->locale    = $locale;
+        $this->minLength = (int) $minLength;
+
+    }
+
+    /**
+     * Vérifie si le texte est un spam ou non
+     *
+     * @param string $text
+     * @return bool
+     */
+    public function isSpam($text)
+    {
+        return strlen($text) < $this->minLength;
+    }
+
+    /**
+     *
+     */
+    public function retourPArameter(){
+
+    }
+}
